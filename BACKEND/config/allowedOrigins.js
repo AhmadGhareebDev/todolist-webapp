1e0c767ae3a978cdc9e05f8669adfddc90f529e4
Ahmad Ghareeb <ahmad.ghareeb0000@gmail.com>
2025-10-05 14:18:00 +0300

    Update allowedOrigins.js

--- BACKEND/config/allowedOrigins.js
+++ BACKEND/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-
-    'http://localhost:5173',
-    'https://todolist-frontend.onrender.com'
+  'http://localhost:5173',
+  'https://todolist-frontend-8jjl.onrender.com',
 ];
-
 module.exports = allowedOrigins;
